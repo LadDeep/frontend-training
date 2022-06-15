a14ad6899edf77dc7c936389ff6486602dc7010e
Deep Lad <deeplad2000@gmail.com>
2022-06-15 10:13:07 +0530

    Deferring js to eliminate onload execution

--- DOM Creation/index.js
+++ DOM Creation/index.js
@@ -16,19 +16,18 @@ const appList = [
         source:"images/hulu.png"
     }
 ];
-window.addEventListener("load", ()=>{
-    const listEle = document.getElementById('apps');
-    console.log(listEle);
-    for (const app of appList) {
-        let listItem = document.createElement('li');
-        let listItemContainer = document.createElement('span');
-        let companyLogo = document.createElement('img');
-        let companyName = document.createTextNode(app.name); 
-        companyLogo.src = app.source;
-        listItemContainer.classList.add('flex-container');
-        listItemContainer.append(companyLogo);
-        listItemContainer.append(companyName);
-        listItem.appendChild(listItemContainer);
-        listEle.appendChild(listItem);
-    }
-})
+
+const listEle = document.getElementById('apps');
+console.log(listEle);
+for (const app of appList) {
+    let listItem = document.createElement('li');
+    let listItemContainer = document.createElement('span');
+    let companyLogo = document.createElement('img');
+    let companyName = document.createTextNode(app.name); 
+    companyLogo.src = app.source;
+    listItemContainer.classList.add('flex-container');
+    listItemContainer.append(companyLogo);
+    listItemContainer.append(companyName);
+    listItem.appendChild(listItemContainer);
+    listEle.appendChild(listItem);
+}
